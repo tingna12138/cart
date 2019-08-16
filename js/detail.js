3315e4722d9b600d051c65eb9ef8f6153945aaa1
LovlyGir <lovlygir@163.com>
2019-08-16 11:30:36 +0800

    商品详情页面---加减商品数量部分

--- js/detail.js
+++ js/detail.js
@@ -24,5 +24,41 @@ $('.choose-item .dd a').on('click',function(){
 })
 
 //3.加减商品数量部分
+//3.1点击加号，文本框中的数值加1
+$('.choose-amount .add').on('click',function(){
+  $(this).prev().val( $(this).prev().val()-0+1);
+  $(this).next().removeClass('disabled');
+})
+//3.2点击减号，文本框中的数值减1
+$('.choose-amount .reduce').on('click',function(){
+   //3.2.1当点击减号后，文本框中的数值为1时的情况：不能点，且鼠标样式为禁用状态
+   //3.2.2当没有进行任何操作，，文本框中的数值为1时的情况----这种情况只能在打开页面时有
+   if($(this).siblings('input').val()>1){
+     $(this).siblings('input').val( $(this).siblings('input').val()-1);
+   }
+   if($(this).siblings('input').val()<=1){
+     $(this).siblings('input').val('1');
+      $(this).addClass('disabled');
+   }
+})
+//3.3在文本框手动输入数值
+$('.choose-amount .choose-number').on('blur',function(){
+  if($(this).val()<=1){
+    $(this).val('1');
+  }else{
+    $(this).siblings('.reduce').removeClass('disabled');
+  }
+})
+
 
 //4.点击购物车触发跳转事件
+//4.1点击购物车，将页面的一些商品参数储存到内存中*********可修改
+
+//4.2跳转页面
+
+
+
+
+
+// 总结：1.javascript:;与javascript:void(0);的区别
+      //2.数量加减部分的禁用样式仅仅只是改变了鼠标的样式而已，并不能真正禁用a标签
