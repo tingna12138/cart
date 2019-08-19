3ee1d5fbac2253789c06d9b146389c8ae30e3d08
LovlyGir <lovlygir@163.com>
2019-08-19 17:46:46 +0800

    商品详情页--添加放大镜效果

--- js/detail.js
+++ js/detail.js
@@ -20,14 +20,56 @@ $('.choose-btns .addshopcar').on('click', function () {
   var arr = utils.getStorage('selectGoods') || []
   //将商品数据封装成对象
   var obj = {
-    id :arr.length+1,
-    pID:item.pID,
-    counts:$('.choose-amount input').val()
+    id: arr.length + 1,
+    pID: item.pID,
+    counts: $('.choose-amount input').val()
   }
   //将对象追加到内存中
   arr.push(obj);
-  utils.setStorage('selectGoods',arr);
+  utils.setStorage('selectGoods', arr);
 
   //页面跳转
   location.assign('./cart.html');
+})
+
+//功能补充
+//5.添加放大镜效果
+$('.preview-img').hover(function () {
+  //5.1鼠标移入时显示盒子
+  $('.preview-big-img,.preview-img .mask').removeClass('hidden');
+}, function () {
+  //5.2鼠标移出时隐藏盒子
+  $('.preview-big-img,.preview-img .mask').addClass('hidden');
+})
+//5.3当鼠标在盒子里移动时，大盒子里的图片跟随移动
+//5.3.1遮罩盒子的坐标跟随鼠标改变
+$('.preview-img').on('mousemove', function (event) {
+  // console.dir(event)
+  var left, top;
+
+  //鼠标相对于preview-img盒子的坐标,再减去盒子宽高的一半
+  left = event.pageX - $(this).offset().left - $('.preview-img .mask').width() / 2;
+  top = event.pageY - $(this).offset().top - $('.preview-img .mask').height() / 2;
+
+  //设置遮罩盒子的水平定位
+  if (left < 0) {
+    $('.preview-img .mask').css('left', 0)
+  } else if (left > ($(this).width() - $('.preview-img .mask').width())) {
+    $('.preview-img .mask').css('left', ($(this).width() - $('.preview-img .mask').width()))
+  } else {
+    $('.preview-img .mask').css('left', left)
+  }
+  //设置遮罩盒子的竖直定位
+  if (top < 0) {
+    $('.preview-img .mask').css('top', 0)
+  } else if (top > ($(this).height() - $('.preview-img .mask').height())) {
+    $('.preview-img .mask').css('top', ($(this).height() - $('.preview-img .mask').height()))
+  } else {
+    $('.preview-img .mask').css('top', top)
+  }
+  //大盒子图片位置与遮罩盒子对应
+  $('.preview-big-img img').css({
+    left:-parseFloat($('.preview-img .mask').css('left'))*300/100,
+    top:-parseFloat($('.preview-img .mask').css('top'))*300/100,
+  })
 })
